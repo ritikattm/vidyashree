94b445262d3fb019a257ecbae9dc82636b374352
ritikattm <ritika@de-haze.com>
2022-06-27 20:26:24 +0530

    "dockerized my code"

--- app1.py
+++ app1.py
@@ -1,3 +1,8 @@
+from flask import Flask
+app=Flask (__name__)
+@app.route("/")
 def hello():
-	print("hello everyone")
+	return "hello everyone"
+if __name__=="__main__":
+	app.run(host='0.0.0.0')	
 
