08a043ef1202edc147ea2956ef2f03b8b823d165
ritikattm <ritika@de-haze.com>
2022-06-27 17:59:14 +0530

    "vidhyashree branch pushed"

--- app1.py
+++ app1.py
@@ -1,12 +1,3 @@
-from flask import Flask
-
-app=Flask(__name__)
-
-@app.route("/",methods=['GET','POST']
 def hello():
-    if (request.method=="GET'):
-        print("hello everyone")
-
-if __name__="__main__":
-    app.run(port=8000)
+	print("hello everyone")
 
